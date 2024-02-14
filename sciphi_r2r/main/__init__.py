def59bcbc73bf146b07054baec939096baf917e4
emrgnt-cmplxty <owen@algofi.org>
2024-02-14 08:25:35 -0800

    refactor examples

--- sciphi_r2r/main/__init__.py
+++ sciphi_r2r/main/__init__.py
@@ -1,4 +1,5 @@
 from .app import create_app
 from .utils import configure_logging, load_config
+from .worker import get_worker
 
-__all__ = ["create_app", "configure_logging", "load_config"]
+__all__ = ["create_app", "configure_logging", "load_config", "get_worker"]
