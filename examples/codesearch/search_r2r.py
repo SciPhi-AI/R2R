ee6ddb77f72309bd4fe8c04054feb118a7d372c4
emrgnt-cmplxty <owen@algofi.org>
2024-02-21 21:26:16 -0800

    cleanup types

--- examples/codesearch/search_r2r.py
+++ examples/codesearch/search_r2r.py
@@ -14,6 +14,8 @@ base_url = "http://localhost:8000"  # Change this to your actual API base URL
 client = SciPhiR2RClient(base_url)
 dotenv.load_dotenv()
 
+DESCRIPTION_PROMPT = "Summarize the following code snippet in two to three sentences: \n\n{extraction}"
+
 if __name__ == "__main__":
     (
         api_config,
@@ -35,15 +37,14 @@ if __name__ == "__main__":
         do_stream=language_model_config["do_stream"],
     )
 
-    prompt = "Summarize the following code snippet in two to three sentences: \n\n{blob}"
 
     for i, (symbol, extraction) in enumerate(Indexer().extractor()):
         document_id = str(uuid.uuid5(uuid.NAMESPACE_DNS, symbol))
-        document_text = f"Symbol: {symbol}\nExtraction:\n\n{extraction}"
+        symbol_extraction = f"Symbol: {symbol}\nExtraction:\n\n{extraction}"
         summary = llm.get_chat_completion(
             [
                 {"role": "system", "content": "You are a helpful assistant."},
-                {"role": "user", "content": prompt.format(blob=document_text)},
+                {"role": "user", "content": DESCRIPTION_PROMPT.format(extraction=symbol_extraction)},
              
              ],
             generation_config
@@ -53,31 +54,29 @@ if __name__ == "__main__":
         entry_response = client.upsert_entries(
             [
                 {
-                    "document_id": str(uuid.uuid5(uuid.NAMESPACE_DNS, symbol+"-summary-only")),
+                    "document_id": str(uuid.uuid5(uuid.NAMESPACE_DNS, symbol+"-desc-only")),
                     "blobs": {"txt": description},
-                    "metadata": {"symbol": symbol, 'type': 'summary-only'},
+                    "metadata": {"symbol": symbol, 'type': 'desc-only'},
                 },
                 {
-                    "document_id": str(uuid.uuid5(uuid.NAMESPACE_DNS, symbol+"-raw-only")),
-                    "blobs": {"txt": document_text},
-                    "metadata": {"symbol": symbol, 'type': 'raw-only'},
+                    "document_id": str(uuid.uuid5(uuid.NAMESPACE_DNS, symbol+"-ext-only")),
+                    "blobs": {"txt": extraction},
+                    "metadata": {"symbol": symbol, 'type': 'ext-only'},
                 },
                 {
-                    "document_id": str(uuid.uuid5(uuid.NAMESPACE_DNS, symbol+"-summary-plus-raw")),
-                    "blobs": {"txt": f"Description:\n{description}\n{document_text}"},
-                    "metadata": {"symbol": symbol, 'type': 'raw-plus-summary'},
+                    "document_id": str(uuid.uuid5(uuid.NAMESPACE_DNS, symbol+"-symbol-ext-only")),
+                    "blobs": {"txt": symbol_extraction},
+                    "metadata": {"symbol": symbol, 'type': 'symbol-ext-only'},
+                },
+                {
+                    "document_id": str(uuid.uuid5(uuid.NAMESPACE_DNS, symbol+"-desc-plus-symbol-ext")),
+                    "blobs": {"txt": f"Description:\n{description}\n{symbol_extraction}"},
+                    "metadata": {"symbol": symbol, 'type': 'desc-plus-symbol-ext'},
                 }
             ],
             {"embedding_settings": {"do_chunking": "false"}}
         )
 
-
-        print("Upserted entry:", entry_response)
-        break
-
-    # print("Searching remote db...")
-    # search_response = client.search("How do i perform logging?", 20)
-    # for response in search_response:
-    #     print(response['metadata']['text'])
-    # # print(f"Search response:\n{search_response}\n\n")
-
+    print("Searching remote db...")
+    search_response = client.search("How does logging take place?", 20)
+    print(f"Search response:\n{search_response}\n\n")
