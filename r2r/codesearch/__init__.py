0cd156cb7ab83776c162d3137696c6fcc54e465d
emrgnt-cmplxty <owen@algofi.org>
2024-02-21 21:19:38 -0800

    add simple code search ex.

---
+++ r2r/codesearch/__init__.py
@@ -0,0 +1,3 @@
+from .indexer import Indexer
+
+__all__ = ["Indexer"]
---
+++ r2r/codesearch/indexer.py
@@ -0,0 +1,32 @@
+import os
+from r2r.codesearch.scip_pb2 import Index
+
+class Indexer:
+    def __init__(self, path: str = "index.scip", project_name: str = "r2r"):
+        self.path = path
+        self.project_name = project_name
+        self.index = self._load_index_protobuf()
+
+    def _load_index_protobuf(self) -> Index:
+        """
+        Loads and returns an Index protobuf object from the given file path.
+        """
+        index = Index()
+        with open(self.path, "rb") as f:
+            index.ParseFromString(f.read())
+        return index
+
+    def extractor(self):
+        for document in self.index.documents:
+            with open(os.path.join(self.project_name, document.relative_path), 'r') as f:
+                content = f.readlines()
+            for i, occurence in enumerate(document.occurrences):
+                if 'python r2r' not in occurence.symbol:
+                    continue
+                if len(occurence.enclosing_range) != 4:
+                    continue
+                # bounding box coordinates
+                symbol = "".join(occurence.symbol.split('`')[1:])
+                line, col, end_line, end_col = occurence.enclosing_range
+                extraction = "".join(content[line:end_line])
+                yield symbol, extraction
