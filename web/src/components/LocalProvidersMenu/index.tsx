3fecf678d089c012836103b357896514c82b0860
Jan Carbonell <jcllobet@gmail.com>
2024-02-27 23:25:59 -0800

    path + subset to then pass onclick

--- web/src/components/LocalProvidersMenu/index.tsx
+++ web/src/components/LocalProvidersMenu/index.tsx
@@ -1,27 +1,60 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useRouter } from 'next/router';
-import styles from './styles.module.scss'; // Import the styles
+import styles from './styles.module.scss';
+import { Provider } from '@/types';
+import { useProviderDataContext } from '@/context/providerContext'; // Adjust the import path as necessary
+
+interface MenuItem {
+  name: string;
+  path: string;
+  subset: string;
+}
 
 const menuItems = [
-  { name: 'Databases', path: '/providers/databases' },
-  { name: 'Datasets', path: '/providers/datasets' },
-  { name: 'LLMs', path: '/providers/large-language-models' },
-  { name: 'Integrations', path: '/providers/integrations' },
+  {
+    name: 'Databases',
+    path: '/providers/databases',
+    subset: 'vector-db-provider',
+  },
+  {
+    name: 'Datasets',
+    path: '/providers/datasets',
+    subset: 'dataset-provider',
+  },
+  {
+    name: 'LLMs',
+    path: '/providers/large-language-models',
+    subset: 'llm_provider',
+  },
+  {
+    name: 'Integrations',
+    path: '/providers/integrations',
+    subset: 'integration',
+  },
 ];
 
 export default function LocalProvidersMenu() {
   const router = useRouter();
+  const { getAllProviders, getFilteredProviders } = useProviderDataContext();
   const currentPath = router.pathname;
+  const currentMenuItem = menuItems.find((item) => item.path === currentPath);
+  const providers = getAllProviders();
+
+  const handleOnClick = (currentMenuItem) => {
+    router.push(currentMenuItem.path);
+    // console.log('Current Menu Item:', currentMenuItem);
+    // console.log('Routing to:', currentMenuItem.path);
+  };
 
   return (
     <div className={styles.menu}>
-      {menuItems.map((item) => (
+      {menuItems.map((menuItem) => (
         <span
-          key={item.path}
-          className={`${styles.menuItem} ${currentPath === item.path ? styles.active : ''}`}
-          onClick={() => router.push(item.path)}
+          key={menuItem.name}
+          className={styles.menuItem}
+          onClick={() => handleOnClick(menuItem)}
         >
-          {item.name}
+          {menuItem.name}
         </span>
       ))}
     </div>
