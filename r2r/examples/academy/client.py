a56f109f5f4b4d27fb8498c24e54defb0434e541
emrgnt-cmplxty <owen@algofi.org>
2024-02-28 17:38:01 -0800

    fix academy client

--- r2r/examples/academy/client.py
+++ r2r/examples/academy/client.py
@@ -9,8 +9,8 @@ base_url = "http://localhost:8000"  # Change this to your actual API base URL
 client = R2RClient(base_url)
 
 titles = {
-    "examples/academy/meditations.pdf": "Title: Meditations - Marcus Aurelius",
-    "examples/academy/the_republic.pdf": "Title: The Republic - Plato",
+    "meditations.pdf": "Title: Meditations - Marcus Aurelius",
+    "the_republic.pdf": "Title: The Republic - Plato",
 }
 
 user_id_0 = str(uuid.uuid5(uuid.NAMESPACE_DNS, "user_0"))
@@ -20,14 +20,16 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 
 # Use this directory in the glob pattern
 for file_path in glob.glob(os.path.join(current_dir, "*.pdf")):
-    print(f"Uploading and processing file: {file_path}")
-    # # Upload and process a file
-    document_id = str(uuid.uuid5(uuid.NAMESPACE_DNS, file_path))
-    metadata = {"user_id": user_id_0, "chunk_prefix": titles[file_path]}
-    settings: dict = {}
-    upload_response = client.upload_and_process_file(
-        document_id, file_path, metadata, settings
-    )
+    file_name = file_path.split(os.path.sep)[-1]
+    if file_name in titles:
+        print(f"Uploading and processing file: {file_path}")
+        # # Upload and process a file
+        document_id = str(uuid.uuid5(uuid.NAMESPACE_DNS, file_path))
+        metadata = {"user_id": user_id_0, "chunk_prefix": titles[file_name]}
+        settings: dict = {}
+        upload_response = client.upload_and_process_file(
+            document_id, file_path, metadata, settings
+        )
 
 prompt = """You are given a user query {query} and a user context {context}. Use the context to answer the given query. """
 formatted_prompt = prompt.format(
