015d84ea80dcc6e147a6f6a9ba023d20c46b4e88
Jan Carbonell <jcllobet@gmail.com>
2024-02-29 17:52:50 -0800

    moved pipeline to its own page, created index w/ login

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,52 +1,123 @@
-import type { NextPage } from 'next';
 import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
-import { Footer } from '@/components/Footer';
-import Layout from '@/components/Layout';
-import { Card } from '@/components/PipelineCard';
-import { ProjectHeader } from '@/components/ProjectHeader';
+import { createClient } from '@/utils/supabase/component';
 import { Separator } from '@/components/ui/separator';
 
-import styles from '../styles/Index.module.scss';
-import 'react-tippy/dist/tippy.css'
-
-import { Pipeline } from '../types';
-
-const Home: NextPage = () => {
-  const [pipelines, setPipelines] = useState<Pipeline[]>([]);
-  // console.log('pipelines = ', pipelines);
+export default function LoginPage() {
   const router = useRouter();
+  const supabase = createClient();
 
-  useEffect(() => {
-    fetch('/api/pipelines')
-      .then((res) => res.json())
-      .then((json) => setPipelines(json));
-  }, []);
-
-  useEffect(() => {
-    router.push('/events');
-  }, [router]);
-
+  async function signInWithGoogle() {
+    await supabase.auth.signInWithOAuth({
+      provider: 'google',
+      options: {
+        redirectTo: `${window.location.origin}/api/auth/callback`,
+      },
+    });
+  }
   return (
-    <Layout>
-      <main className={styles.main}>
-        <h1 className="text-white text-2xl mb-4"> Pipelines </h1>
-        <Separator />
-        <div className="mt-6" />
-        <ProjectHeader />
-
-        <div className={styles.gridView}>
-          {Array.isArray(pipelines)
-            ? pipelines?.map((pipeline) => (
-                <Card pipeline={pipeline} key={pipeline.id} />
-              ))
-            : null}
+    <>
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <img
+            className="mx-auto h-10 w-auto"
+            src="images/sciphi.png"
+            alt="Your Company"
+          />
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-white">
+            Sign in to your account
+          </h2>
         </div>
-      </main>
-      <Footer />
-    </Layout>
-  );
-};
 
-export default Home;
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form className="space-y-6" action="#" method="POST">
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium leading-6 text-white"
+              >
+                Email address
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  required
+                  className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <div className="flex items-center justify-between">
+                <label
+                  htmlFor="password"
+                  className="block text-sm font-medium leading-6 text-white"
+                >
+                  Password
+                </label>
+                <div className="text-sm">
+                  <a
+                    href="#"
+                    className="font-semibold text-indigo-400 hover:text-indigo-300"
+                  >
+                    Forgot password?
+                  </a>
+                </div>
+              </div>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+              <div className="flex space-x-4">
+                <button
+                  type="button"
+                  onClick={signInWithGoogle}
+                  className="flex w-full justify-center rounded-md bg-white px-3 py-1.5 text-sm font-semibold leading-6 text-indigo-500 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+                >
+                  Sign up
+                </button>
+                <button
+                  type="button"
+                  onClick={signInWithGoogle}
+                  className="flex w-full justify-center rounded-md bg-indigo-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+                >
+                  Log in
+                </button>
+              </div>
+            </div>
+            <Separator className="my-6" />
+            <div>
+              <button
+                type="button"
+                onClick={signInWithGoogle}
+                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              >
+                <span className="absolute left-0 inset-y-0 flex items-center pl-3 ">
+                  <img
+                    src="/images/google.png"
+                    alt="Google Logo"
+                    className="h-5 w-7"
+                  />
+                </span>
+                Sign in with Google
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ web/src/pages/pipeline.tsx
@@ -0,0 +1,48 @@
+import type { NextPage } from 'next';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+
+import { Footer } from '@/components/Footer';
+import Layout from '@/components/Layout';
+import { Card } from '@/components/PipelineCard';
+import { ProjectHeader } from '@/components/ProjectHeader';
+import { Separator } from '@/components/ui/separator';
+
+import styles from '../styles/Index.module.scss';
+import 'react-tippy/dist/tippy.css';
+
+import { Pipeline } from '../types';
+
+const Home: NextPage = () => {
+  const [pipelines, setPipelines] = useState<Pipeline[]>([]);
+  // console.log('pipelines = ', pipelines);
+  const router = useRouter();
+
+  useEffect(() => {
+    fetch('/api/pipelines')
+      .then((res) => res.json())
+      .then((json) => setPipelines(json));
+  }, []);
+
+  return (
+    <Layout>
+      <main className={styles.main}>
+        <h1 className="text-white text-2xl mb-4"> Pipelines </h1>
+        <Separator />
+        <div className="mt-6" />
+        <ProjectHeader />
+
+        <div className={styles.gridView}>
+          {Array.isArray(pipelines)
+            ? pipelines?.map((pipeline) => (
+                <Card pipeline={pipeline} key={pipeline.id} />
+              ))
+            : null}
+        </div>
+      </main>
+      <Footer />
+    </Layout>
+  );
+};
+
+export default Home;
