4a634f03119b8ad0f5dbc077d1813858263708fb
Jan Carbonell <jcllobet@gmail.com>
2024-03-01 17:26:26 -0800

    TBD

--- web/src/pages/update_password.tsx
+++ web/src/pages/update_password.tsx
@@ -1,97 +1,97 @@
-import { useState } from 'react';
-import { useRouter } from 'next/router';
-import { User } from '@supabase/supabase-js';
-import { createClient } from '@/utils/supabase/component';
+// import { useState } from 'react';
+// import { useRouter } from 'next/router';
+// import { User } from '@supabase/supabase-js';
+// import { createClient } from '@/utils/supabase/component';
 
-export default function UpdatePasswordPage({ user }: { user: User }) {
-  const [password, setPassword] = useState('');
-  const [passwordConfirm, setPasswordConfirm] = useState('');
-  const router = useRouter();
-  const supabase = createClient();
+// export default function UpdatePasswordPage({ user }: { user: User }) {
+//   const [password, setPassword] = useState('');
+//   const [passwordConfirm, setPasswordConfirm] = useState('');
+//   const router = useRouter();
+//   const supabase = createClient();
 
-  const updatePassword = async () => {
-    if (password !== passwordConfirm) {
-      alert('Passwords do not match!');
-      return;
-    }
+//   const updatePassword = async () => {
+//     if (password !== passwordConfirm) {
+//       alert('Passwords do not match!');
+//       return;
+//     }
 
-    const { error } = await supabase.auth.updateUser({ password: password });
-    if (error) {
-      console.error(error);
-    } else {
-      router.push('/');
-    }
-  };
+//     const { error } = await supabase.auth.updateUser({ password: password });
+//     if (error) {
+//       console.error(error);
+//     } else {
+//       router.push('/');
+//     }
+//   };
 
-  return (
-    <>
-      <div className="flex min-h-screen items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-        <div className="w-full max-w-md space-y-8">
-          <div>
-            <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-              Update Your Password
-            </h2>
-          </div>
-          <form className="mt-8 space-y-6" action="#" method="POST">
-            <input type="hidden" name="remember" value="true" />
-            <div className="rounded-md shadow-sm -space-y-px">
-              <div>
-                <label htmlFor="password" className="sr-only">
-                  New Password
-                </label>
-                <input
-                  id="password"
-                  name="password"
-                  type="password"
-                  autoComplete="new-password"
-                  required
-                  className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                  placeholder="New password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-              </div>
-              <div>
-                <label htmlFor="password-confirm" className="sr-only">
-                  Confirm Password
-                </label>
-                <input
-                  id="password-confirm"
-                  name="password-confirm"
-                  type="password"
-                  autoComplete="new-password"
-                  required
-                  className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                  placeholder="Confirm new password"
-                  value={passwordConfirm}
-                  onChange={(e) => setPasswordConfirm(e.target.value)}
-                />
-              </div>
-            </div>
+//   return (
+//     <>
+//       <div className="flex min-h-screen items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+//         <div className="w-full max-w-md space-y-8">
+//           <div>
+//             <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+//               Update Your Password
+//             </h2>
+//           </div>
+//           <form className="mt-8 space-y-6" action="#" method="POST">
+//             <input type="hidden" name="remember" value="true" />
+//             <div className="rounded-md shadow-sm -space-y-px">
+//               <div>
+//                 <label htmlFor="password" className="sr-only">
+//                   New Password
+//                 </label>
+//                 <input
+//                   id="password"
+//                   name="password"
+//                   type="password"
+//                   autoComplete="new-password"
+//                   required
+//                   className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+//                   placeholder="New password"
+//                   value={password}
+//                   onChange={(e) => setPassword(e.target.value)}
+//                 />
+//               </div>
+//               <div>
+//                 <label htmlFor="password-confirm" className="sr-only">
+//                   Confirm Password
+//                 </label>
+//                 <input
+//                   id="password-confirm"
+//                   name="password-confirm"
+//                   type="password"
+//                   autoComplete="new-password"
+//                   required
+//                   className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+//                   placeholder="Confirm new password"
+//                   value={passwordConfirm}
+//                   onChange={(e) => setPasswordConfirm(e.target.value)}
+//                 />
+//               </div>
+//             </div>
 
-            {password !== passwordConfirm && (
-              <p className="mt-2 text-center text-sm text-red-600">
-                Passwords do not match
-              </p>
-            )}
+//             {password !== passwordConfirm && (
+//               <p className="mt-2 text-center text-sm text-red-600">
+//                 Passwords do not match
+//               </p>
+//             )}
 
-            <div>
-              <button
-                type="submit"
-                onClick={updatePassword}
-                disabled={password !== passwordConfirm}
-                className={`group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white ${
-                  password === passwordConfirm
-                    ? 'bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500'
-                    : 'bg-gray-300 text-gray-500 cursor-not-allowed'
-                } focus:outline-none focus:ring-2 focus:ring-offset-2`}
-              >
-                Update Password
-              </button>
-            </div>
-          </form>
-        </div>
-      </div>
-    </>
-  );
-}
+//             <div>
+//               <button
+//                 type="submit"
+//                 onClick={updatePassword}
+//                 disabled={password !== passwordConfirm}
+//                 className={`group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white ${
+//                   password === passwordConfirm
+//                     ? 'bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500'
+//                     : 'bg-gray-300 text-gray-500 cursor-not-allowed'
+//                 } focus:outline-none focus:ring-2 focus:ring-offset-2`}
+//               >
+//                 Update Password
+//               </button>
+//             </div>
+//           </form>
+//         </div>
+//       </div>
+//     </>
+//   );
+// }
