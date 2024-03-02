884d2d4a71c90039b5bd069ffbf99da8062a0f32
Jan Carbonell <jcllobet@gmail.com>
2024-03-01 17:26:17 -0800

    login page

--- web/src/pages/login.tsx
+++ web/src/pages/login.tsx
@@ -2,10 +2,20 @@ import { useRouter } from 'next/router';
 import { useState } from 'react';
 
 import { createClient } from '@/utils/supabase/component';
+import { Separator } from '@/components/ui/separator';
 
 export default function LoginPage() {
   const router = useRouter();
   const supabase = createClient();
+
+  async function signInWithOAuth(provider: 'google' | 'github') {
+    await supabase.auth.signInWithOAuth({
+      provider,
+      options: {
+        redirectTo: `${window.location.origin}/api/auth/callback`,
+      },
+    });
+  }
 
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -29,30 +39,137 @@ export default function LoginPage() {
     router.push('/');
   }
 
+  // async function resetPassword() {
+  //   const { error } = await supabase.auth.resetPasswordForEmail(email, {
+  //     redirectTo: `${window.location.origin}/update-password`,
+  //   });
+  //   if (error) {
+  //     console.error(error);
+  //   }
+  // }
   return (
-    <main>
-      <form>
-        <label htmlFor="email">Email:</label>
-        <input
-          id="email"
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <label htmlFor="password">Password:</label>
-        <input
-          id="password"
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button type="button" onClick={logIn}>
-          Log in
-        </button>
-        <button type="button" onClick={signUp}>
-          Sign up
-        </button>
-      </form>
-    </main>
+    <>
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <img
+            className="mx-auto h-10 w-auto"
+            src="images/sciphi.png"
+            alt="Your Company"
+          />
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-white">
+            Sign in to your account
+          </h2>
+        </div>
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form className="space-y-6" action="#" method="POST">
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium leading-6 text-white"
+              >
+                Email address
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  required
+                  className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+            </div>
+
+            <div>
+              <div className="flex items-center justify-between">
+                <label
+                  htmlFor="password"
+                  className="block text-sm font-medium leading-6 text-white"
+                >
+                  Password
+                </label>
+                {/* <div className="text-sm">
+                  <a
+                    href="#"
+                    className="font-semibold text-indigo-400 hover:text-indigo-300"
+                    onClick={resetPassword}
+                  >
+                    Forgot password?
+                  </a>
+                </div> */}
+              </div>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+            </div>
+
+            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+              <div className="flex space-x-4">
+                <button
+                  type="button"
+                  onClick={signUp}
+                  className="flex w-full justify-center rounded-md bg-white px-3 py-1.5 text-sm font-semibold leading-6 text-indigo-500 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+                >
+                  Sign up
+                </button>
+                <button
+                  type="button"
+                  onClick={logIn}
+                  className="flex w-full justify-center rounded-md bg-indigo-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+                >
+                  Log in
+                </button>
+              </div>
+            </div>
+            <Separator className="my-6" />
+            <div>
+              <button
+                type="button"
+                onClick={() => signInWithOAuth('google')}
+                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              >
+                <span className="absolute left-0 inset-y-0 flex items-center pl-3 ">
+                  <img
+                    src="/images/google.png"
+                    alt="Google Logo"
+                    className="h-5 w-7"
+                  />
+                </span>
+                Sign in with Google
+              </button>
+            </div>
+            <div>
+              <button
+                type="button"
+                onClick={() => signInWithOAuth('github')}
+                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              >
+                <span className="absolute left-0 inset-y-0 flex items-center pl-3 ">
+                  <img
+                    src="/images/github.png"
+                    alt="Github Logo"
+                    className="h-6 w-6"
+                  />
+                </span>
+                Sign in with Github
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
   );
 }
