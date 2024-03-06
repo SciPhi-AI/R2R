e43620803bc310302f4876b6580bd4eaad7d685a
emrgnt-cmplxty <owen@algofi.org>
2024-03-05 21:50:18 -0800

    cleanup

--- web/src/components/PipelineCard/index.tsx
+++ web/src/components/PipelineCard/index.tsx
@@ -4,65 +4,11 @@ import { FiExternalLink } from 'react-icons/fi';
 import styles from './styles.module.scss';
 import { Pipeline } from '../../types';
 
-
-// interface ModalProps {
-//   isOpen: boolean;
-//   onClose: () => void;
-//   children: React.ReactNode;
-// }
-
-// const Modal: React.FC<ModalProps> = ({ isOpen, onClose, children }) => {
-//   const modalRef = useRef<HTMLDivElement>(null);
-
-//   useEffect(() => {
-//     const handleOutsideClick = (event: MouseEvent) => {
-//       if (modalRef.current && !modalRef.current.contains(event.target as Node)) {
-//         onClose();
-//       }
-//     };
-
-//     if (isOpen) {
-//       document.addEventListener('mousedown', handleOutsideClick);
-//     }
-
-//     return () => {
-//       document.removeEventListener('mousedown', handleOutsideClick);
-//     };
-//   }, [isOpen, onClose]);
-
-//   if (!isOpen) return null;
-
-//   return (
-//     <div className={styles.modalOverlay}>
-//       <div className={styles.modalContent} ref={modalRef}>
-//         {children}
-//       </div>
-//     </div>
-//   );
-// };
-
-
 interface CardProps {
   pipeline: Pipeline;
 }
 
 function Card({ pipeline }: CardProps) {
-  // const [isModalOpen, setIsModalOpen] = useState(false);
-
-  // console.log('isModalOpen = ', isModalOpen)
-  // const handleCardClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
-  //   e.preventDefault(); // Prevent the default anchor action
-  //   setIsModalOpen(true); // Open the modal
-  // };
-  // useEffect(() => {
-  //   console.log('Modal state updated, isModalOpen = ', isModalOpen);
-  // }, [isModalOpen]); // This will log whenever isModalOpen changes
-  // const handleCloseModal = () => {
-  //   console.log('handleCloseModal called');
-  //   setIsModalOpen(false);
-  // };
-
-
   return (
     <a href="#" className={styles.container}  >
       <div className={styles.cardHeader}>
