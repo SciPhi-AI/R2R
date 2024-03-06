87370115d4b849a9d3b7f5f3b109535c715c36cc
Jan Carbonell <jcllobet@gmail.com>
2024-03-06 02:02:29 -0500

    working on confirm_pass updates

--- web/src/pages/api/auth/confirm.ts
+++ web/src/pages/api/auth/confirm.ts
@@ -5,22 +5,22 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  console.log('Received request at /api/auth/confirm');
-  console.log('Request query:', req.query);
+  // console.log('Received request at /api/auth/confirm');
+  // console.log('Request query:', req.query);
 
   const { token_hash, next, email } = req.query;
 
   if (!token_hash) {
-    console.log('Token hash is missing');
+    // console.log('Token hash is missing');
     res.status(400).json({ error: 'Token hash is missing' });
     return;
   }
 
-  console.log('Token hash is present');
-  console.log(
-    'Redirecting to:',
-    `/update_password?token_hash=${token_hash}&email=${email}&next=${next || '/'}`
-  );
+  // console.log('Token hash is present');
+  // console.log(
+  //   'Redirecting to:',
+  //   `/update_password?token_hash=${token_hash}&email=${email}&next=${next || '/'}`
+  // );
 
   res.redirect(
     307,
