e5920e320a7ecd46582b2ba9d2d94a34783a37dc
Jan Carbonell <jcllobet@gmail.com>
2024-03-05 21:12:01 -0500

    chasing bug on /update_password

--- web/src/utils/supabase/api.ts
+++ web/src/utils/supabase/api.ts
@@ -1,24 +1,34 @@
-import { createServerClient, type CookieOptions, serialize } from '@supabase/ssr'
-import { type NextApiRequest, type NextApiResponse } from 'next'
+import {
+  createServerClient,
+  type CookieOptions,
+  serialize,
+} from '@supabase/ssr';
+import { type NextApiRequest, type NextApiResponse } from 'next';
 
-export default function createClient(req: NextApiRequest, res: NextApiResponse) {
+export default function createClient(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   const supabase = createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
         get(name: string) {
-          return req.cookies[name]
+          return req.cookies[name];
         },
         set(name: string, value: string, options: CookieOptions) {
-          res.appendHeader('Set-Cookie', serialize(name, value, options))
+          res.appendHeader('Set-Cookie', serialize(name, value, options));
         },
         remove(name: string, options: CookieOptions) {
-          res.appendHeader('Set-Cookie', serialize(name, '', options))
+          res.appendHeader('Set-Cookie', serialize(name, '', options));
         },
       },
     }
-  )
-
-  return supabase
+  );
+  console.log('Supabase Client Created');
+  console.log('Supabase:', supabase);
+  console.log('Request:', req);
+  console.log('Response:', res);
+  return supabase;
 }
