2049f81c4ed7aab729584f81649058665be82e6f
emrgnt-cmplxty <owen@algofi.org>
2024-03-06 21:05:57 -0800

    Add dropdown menu

--- web/src/components/MainMenu/index.tsx
+++ web/src/components/MainMenu/index.tsx
@@ -1,35 +1,179 @@
+import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
 import Image from 'next/image';
-import Link from 'next/link';
-
 import styles from './styles.module.scss';
 import { WorkspacesSelect } from '../WorkspacesSelect';
+import { createClient } from '@/utils/supabase/component';
+import {
+  Bug,
+  Cloud,
+  CreditCard,
+  Github,
+  Keyboard,
+  LifeBuoy,
+  LogOut,
+  Mail,
+  MessageSquare,
+  Plus,
+  PlusCircle,
+  Settings,
+  User,
+  UserPlus,
+  Users,
+} from "lucide-react"
 
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuPortal,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+ 
 export function MainMenu() {
+  const supabase = createClient();
+  const [user, setUser] = useState(null);
+  const router = useRouter();
+
+  useEffect(() => {
+      supabase.auth.getSession().then(({ data: { session } }) => {
+        console.log('session = ', session)
+        setUser(session?.user);
+      });
+  
+  }, []);
+
+  const handleLogout = async () => {
+    const { error } = await supabase.auth.signOut();
+    if (!error) {
+      // Redirect to the login page or home page after logout
+      router.push('/login');
+    } else {
+      // Handle error case
+      console.error('Logout failed: ', error);
+    }
+  };
+
   return (
     <div className={styles.container}>
       <WorkspacesSelect />
 
       <div className={styles.leftMainMenuNavigation}>
-        {/* <button>Feedback</button>
-        <button>Changelog</button>
-        <button>Support</button> */}
-        <Link
-          href="https://docs.sciphi.ai"
+        <a
           style={{ cursor: 'pointer' }}
+          href="https://docs.sciphi.ai"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Docs
-        </Link>
+          <button>Docs</button>
+        </a>
 
         <div>
-          <Image
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+      <Image
             src="/images/dummy_logo.png"
             alt="Acme Co."
             width="28"
             height="28"
             className={styles.userIcon}
           />
+
+        {/* <Button variant="outline">Open</Button> */}
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56">
+        <DropdownMenuLabel>{user === null ? 'My Account' : user.email}</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuGroup>
+          <DropdownMenuItem className={styles.handCursor}>
+            <User className="mr-2 h-4 w-4" />
+            <span>Profile</span>
+            <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
+          </DropdownMenuItem>
+          <DropdownMenuItem className={styles.handCursor} disabled={true}>
+            <CreditCard className="mr-2 h-4 w-4" />
+            <span>Billing</span>
+            <DropdownMenuShortcut>⌘B</DropdownMenuShortcut>
+          </DropdownMenuItem>
+          <DropdownMenuItem className={styles.handCursor}>
+            <Settings className="mr-2 h-4 w-4" />
+            <span>Settings</span>
+            <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
+          </DropdownMenuItem>
+          {/* <DropdownMenuItem>
+            <Keyboard className="mr-2 h-4 w-4" />
+            <span>Keyboard shortcuts</span>
+            <DropdownMenuShortcut>⌘K</DropdownMenuShortcut>
+          </DropdownMenuItem> */}
+        </DropdownMenuGroup>
+        <DropdownMenuSeparator />
+        {/* <DropdownMenuGroup>
+          <DropdownMenuItem>
+            <Users className="mr-2 h-4 w-4" />
+            <span>Team</span>
+          </DropdownMenuItem>
+          <DropdownMenuSub>
+            <DropdownMenuSubTrigger>
+              <UserPlus className="mr-2 h-4 w-4" />
+              <span>Invite users</span>
+            </DropdownMenuSubTrigger>
+            <DropdownMenuPortal>
+              <DropdownMenuSubContent>
+                <DropdownMenuItem>
+                  <Mail className="mr-2 h-4 w-4" />
+                  <span>Email</span>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <MessageSquare className="mr-2 h-4 w-4" />
+                  <span>Message</span>
+                </DropdownMenuItem>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>
+                  <PlusCircle className="mr-2 h-4 w-4" />
+                  <span>More...</span>
+                </DropdownMenuItem>
+              </DropdownMenuSubContent>
+            </DropdownMenuPortal>
+          </DropdownMenuSub>
+          <DropdownMenuItem>
+            <Plus className="mr-2 h-4 w-4" />
+            <span>New Team</span>
+            <DropdownMenuShortcut>⌘+T</DropdownMenuShortcut>
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+        <DropdownMenuSeparator /> */}
+        {/* <DropdownMenuItem>
+          <Github className="mr-2 h-4 w-4" />
+          <span>GitHub</span>
+        </DropdownMenuItem> */}
+        <DropdownMenuItem className={styles.handCursor}>
+          <Bug className="mr-2 h-4 w-4" />
+          <span>Report a bug</span>
+        </DropdownMenuItem>
+        <DropdownMenuItem className={styles.handCursor}>
+          <LifeBuoy className="mr-2 h-4 w-4" />
+          <span>Support</span>
+        </DropdownMenuItem>
+        <DropdownMenuItem disabled>
+          <Cloud className="mr-2 h-4 w-4" />
+          <span>API</span>
+        </DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem onClick={handleLogout} className={styles.handCursor}>
+          <LogOut className="mr-2 h-4 w-4" />
+          <span>Log out</span>
+          <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+
         </div>
       </div>
     </div>
