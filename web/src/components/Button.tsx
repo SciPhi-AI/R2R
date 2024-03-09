34a8da632704bc7c324fba4eaf9d81fa956a8abb
Jan Carbonell <jcllobet@gmail.com>
2024-03-09 15:10:06 -0500

    playing around with buttons

---
+++ web/src/components/Button.tsx
@@ -0,0 +1,37 @@
+import Link from 'next/link';
+import clsx from 'clsx';
+
+const variantStyles = {
+  primary:
+    'rounded-full bg-zinc-900 py-1 px-3 text-white hover:bg-zinc-700 dark:bg-indigo-400/10 dark:text-indigo-400 dark:ring-1 dark:ring-inset dark:ring-indigo-400/20 dark:hover:bg-indigo-400/10 dark:hover:text-indigo-300 dark:hover:ring-indigo-300',
+  secondary:
+    'rounded-full py-1 px-3 text-zinc-900 hover:bg-zinc-200 dark:bg-zinc-800/40 dark:text-zinc-400 dark:ring-1 dark:ring-inset dark:ring-zinc-800 dark:hover:bg-zinc-800 dark:hover:text-zinc-300',
+  filled:
+    'rounded-full bg-zinc-900 py-1 px-3 text-white hover:bg-zinc-700 dark:bg-indigo-600 dark:text-white dark:hover:bg-indigo-500',
+  outline:
+    'rounded-full py-1 px-3 text-zinc-700 ring-1 ring-inset ring-zinc-900/10 hover:bg-zinc-900/2.5 hover:text-zinc-900 dark:text-zinc-400 dark:ring-white/10 dark:hover:bg-white/5 dark:hover:text-white',
+  text: 'text-indigo-500 hover:text-indigo-600 dark:text-indigo-400 dark:hover:text-indigo-500',
+};
+
+type ButtonProps = {
+  variant?: keyof typeof variantStyles;
+} & React.ComponentPropsWithoutRef<'button'>;
+
+export function Button({
+  variant = 'primary',
+  className,
+  children,
+  ...props
+}: ButtonProps) {
+  className = clsx(
+    'inline-flex gap-0.5 justify-center overflow-hidden text-sm font-medium transition',
+    variantStyles[variant],
+    className
+  );
+
+  return (
+    <button className={className} {...props}>
+      {children}
+    </button>
+  );
+}
--- web/src/components/CreatePipelineHeader/index.tsx
+++ web/src/components/CreatePipelineHeader/index.tsx
@@ -1,6 +1,6 @@
-import styles from './styles.module.scss';
 import React from 'react';
 import { useRouter } from 'next/router';
+import { Button } from '@/components/Button';
 
 export function CreatePipelineHeader({
   numPipelines,
@@ -15,6 +15,31 @@ export function CreatePipelineHeader({
 
   return (
     <div className="flex gap-4">
+      <Button className="rounded-md h-10 w-40 py-2" onClick={createPipeline}>
+        New Pipeline
+      </Button>
+
+      <Button
+        className="rounded-md h-10 w-40 py-2"
+        variant="filled"
+        onClick={createPipeline}
+      >
+        New Pipeline
+      </Button>
+      <Button
+        className="rounded-md h-10 w-40 py-2"
+        variant="outline"
+        onClick={createPipeline}
+      >
+        New Pipeline
+      </Button>
+      <Button
+        className="rounded-md h-10 w-40 py-2"
+        variant="text"
+        onClick={createPipeline}
+      >
+        Text Button
+      </Button>
       <button
         className="bg-primary-custom text-color8 border border-color3 rounded-md h-10 w-40 px-2 py-1 text-md outline-none cursor-pointer transition-all duration-200 text-center hover:opacity-90 hover:border-solid hover:border-6 hover:border-color3 hover:text-color8"
         onClick={createPipeline}
