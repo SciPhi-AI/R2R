336544afe367d31906488d4d6129ae405ff3c521
Jan Carbonell <jcllobet@gmail.com>
2024-03-10 19:03:06 -0400

    light / dark mode

--- web/src/components/Footer.tsx
+++ web/src/components/Footer.tsx
@@ -42,7 +42,7 @@ function SocialLink({
   return (
     <Link href={href} className="group">
       <span className="sr-only">{children}</span>
-      <Icon className="h-5 w-5 fill-zinc-700 transition group-hover:fill-zinc-900 dark:group-hover:fill-indigo-600" />
+      <Icon className="h-5 w-5 fill-zinc-700 transition group-hover:fill-indigo-600 dark:group-hover:fill-indigo-600" />
     </Link>
   );
 }
