5565bd75631fcf47e213d79783c92dbe6f90faa8
Jan Carbonell <jcllobet@gmail.com>
2024-03-11 20:45:21 -0400

    fixed navbar header

--- web/src/components/DynamicHeaderPath.tsx
+++ web/src/components/DynamicHeaderPath.tsx
@@ -7,12 +7,6 @@ import { useRouter } from 'next/router';
 import { Pipeline } from '@/types';
 
 const DynamicHeaderPath = ({ user }) => {
-  const [isScrolling, setIsScrolling] = useState(false);
-  const [navItemHighlightProps, setNavItemHighlightProps] = useState<{
-    width: number;
-    translateX: number;
-    translateY?: number;
-  } | null>(null);
   const [pipelines, setPipelines] = useState<Pipeline[]>([]);
   const pipelinesRef = useRef(pipelines);
   const router = useRouter();
@@ -21,32 +15,26 @@ const DynamicHeaderPath = ({ user }) => {
     user && user.email ? user.email.split('@')[0] : 'workspace';
 
   const isPipelineRoute = router.pathname.includes('/pipeline/');
-  console.log('isPipelineRoute: ', isPipelineRoute);
-  console.log('router: ', router.query);
-  const { pipelineName } = isPipelineRoute
-    ? router.query
-    : { pipelineName: '4242' };
-
-  const pipelineShortId = pipelineName
-    ? (pipelineName as string).substring(0, 4)
-    : '1234';
-
-  const determineTextBasedOnUrl = () => {
-    const path = router.asPath;
-    const match = path.match(/\/pipeline\/([^\/]+)\/?(.*)?/);
-    return (match && match[2] ? match[2] : 'idx') + '.txt';
-  };
+  const pathSegments = isPipelineRoute
+    ? router.asPath.split('/').filter(Boolean)
+    : [];
+  const pipelineId = pathSegments.length > 1 ? pathSegments[1] : null;
+  const afterPipelineSegment = pathSegments.length > 2 ? pathSegments[2] : null;
 
   useEffect(() => {
     pipelinesRef.current = pipelines;
   }, [pipelines]);
 
+  const redirectToHome = () => {
+    router.push('/');
+  };
+
   return (
     <div>
       <ul role="list" className="flex items-center gap-3">
         <Logo width={27} height={27} />
-        <Code>
-          <span className="text-zinc-800 dark:text-zinc-400">r2r_rag </span>/{' '}
+        <Code onClick={redirectToHome} style={{ cursor: 'pointer' }}>
+          <span className="text-zinc-800 dark:text-zinc-400">r2r_rag </span>|{' '}
         </Code>
         <div
           aria-label="Home"
@@ -56,24 +44,21 @@ const DynamicHeaderPath = ({ user }) => {
               'linear-gradient(90deg, #1f005c, #5b0060, #870160, #ac255e, #ca485c, #e16b5c, #f39060, #ffb56b)',
           }}
         ></div>
-        <Code>
+        <Code onClick={redirectToHome} style={{ cursor: 'pointer' }}>
           <span className="text-indigo-500">{userNameOrWorkspace}</span>
         </Code>
         {isPipelineRoute && (
           <>
-            <Code>|</Code>
-            <Code>
-              <span className="text-zinc-800 dark:text-zinc-400">grep</span>
-            </Code>
+            <Code>{`>>`}</Code>
             <Code>
               <span className="text-indigo-500">
-                pipeline-{pipelineShortId}...
+                {afterPipelineSegment
+                  ? `${afterPipelineSegment}:`
+                  : 'pipeline:'}
               </span>
             </Code>
             <Code>
-              <span className="text-zinc-800 dark:text-zinc-400">
-                {`>> ${determineTextBasedOnUrl()}`}
-              </span>
+              <span className="text-zinc-400">{pipelineId}</span>
             </Code>
           </>
         )}
