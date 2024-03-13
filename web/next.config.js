8a224c3a94f0da3e208580faf46933f0f15cd168
emrgnt-cmplxty <owen@algofi.org>
2024-03-12 22:17:26 -0700

    test

--- web/next.config.js
+++ web/next.config.js
@@ -2,16 +2,25 @@ module.exports = {
   reactStrictMode: false,
   images: {
     domains: ['github.com', 'lh3.googleusercontent.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'r2r-cloud-docs.vercel.app',
+        port: '',
+        pathname: '/**',
+      },
+    ],
   },
+
   async rewrites() {
     return [
       {
         source: '/docs',
-        destination: '/docs/index.html',
+        destination: 'https://r2r-cloud-docs.vercel.app/',
       },
       {
         source: '/docs/:path*',
-        destination: '/docs/:path*.html',
+        destination: 'https://r2r-cloud-docs.vercel.app/:path*',
       },
     ];
   },
