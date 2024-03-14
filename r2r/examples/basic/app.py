27a49da4f70ee184d8e15118701258e5690d17be
emrgnt-cmplxty <owen@algofi.org>
2024-03-14 00:11:29 -0700

    add changes

--- r2r/examples/basic/app.py
+++ r2r/examples/basic/app.py
@@ -1,6 +1,7 @@
-from r2r.main import E2EPipelineFactory
+from r2r.main import E2EPipelineFactory, load_config
 
 # Creates a pipeline with default configuration
 # This is the main entry point for the application
 # The pipeline is built using the `config.json` file
+config = load_config()
 app = E2EPipelineFactory.create_pipeline()
