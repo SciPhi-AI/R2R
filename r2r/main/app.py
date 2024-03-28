2e20e482e908751593792bed8e41a8b4e74fe37c
emrgnt-cmplxty <68796651+emrgnt-cmplxty@users.noreply.github.com>
2024-03-27 17:57:17 -0700

    Feature/try sync eval (#229)
    * try remove logging conf
    
    * rollbk
    
    * rollbk
    
    * sync eval

--- r2r/main/app.py
+++ r2r/main/app.py
@@ -324,9 +324,10 @@ def create_app(
                     logger.info(
                         f"Performing evaluation with payload: {payload}"
                     )
-                    background_tasks.add_task(
-                        requests.get, f"{url}/eval", json=payload
-                    )
+                    # background_tasks.add_task(
+                    #     requests.get, f"{url}/eval", json=payload
+                    # )
+                    requests.get(f"{url}/eval", json=payload)
                     logger.info(
                         f"Completed evaluation with payload: {payload}"
                     )
