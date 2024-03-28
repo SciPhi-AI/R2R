0a09879640260324775a33087f48949b0ea37db2
emrgnt-cmplxty <owen@algofi.org>
2024-03-27 17:45:35 -0700

    try remove logging conf

--- r2r/main/app.py
+++ r2r/main/app.py
@@ -43,7 +43,7 @@ from .models import (
 )
 
 logger = logging.getLogger("r2r")
-logger.setLevel(logging.INFO)
+logger.basicConfig(level=logging.INFO)
 
 # Current directory where this script is located
 CURRENT_DIR = Path(__file__).resolve().parent
