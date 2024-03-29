d264057cd1576d06226447a0bcef241c0a76d4f1
emrgnt-cmplxty <68796651+emrgnt-cmplxty@users.noreply.github.com>
2024-03-28 22:54:50 -0700

    Update app.py

--- r2r/examples/academy/app.py
+++ r2r/examples/academy/app.py
@@ -1,19 +1,14 @@
+import json
 import logging
-from typing import Optional
+from typing import Optional, Generator
 
-from r2r.core import (
-    GenerationConfig,
-    LLMProvider,
-    LoggingDatabaseConnection,
-    PromptProvider,
-    RAGPipelineOutput,
-    VectorDBProvider,
-    VectorSearchResult,
-    log_execution_to_db,
-)
+from r2r.core import (RAGPipeline, GenerationConfig, LLMProvider, LoggingDatabaseConnection,
+                      RAGPipelineOutput, VectorDBProvider, VectorSearchResult,
+                      log_execution_to_db)
 from r2r.embeddings import OpenAIEmbeddingProvider
 from r2r.main import E2EPipelineFactory, R2RConfig
-from r2r.pipelines import BasicPromptProvider, BasicRAGPipeline
+from r2r.pipelines import BasicRAGPipeline, BasicPromptProvider
+
 
 logger = logging.getLogger(__name__)
 
@@ -44,9 +39,8 @@ class SyntheticRAGPipeline(BasicRAGPipeline):
         embedding_model: str,
         embeddings_provider: OpenAIEmbeddingProvider,
         logging_connection: Optional[LoggingDatabaseConnection] = None,
-        prompt_provider: Optional[PromptProvider] = BasicPromptProvider(
-            DEFAULT_SYSTEM_PROMPT, DEFAULT_TASK_PROMPT
-        ),
+        system_prompt: Optional[str] = DEFAULT_SYSTEM_PROMPT,
+        task_prompt: Optional[str] = DEFAULT_TASK_PROMPT,
     ) -> None:
         logger.debug(f"Initalizing `SyntheticRAGPipeline`")
 
@@ -56,7 +50,7 @@ class SyntheticRAGPipeline(BasicRAGPipeline):
             embedding_model,
             embeddings_provider,
             logging_connection=logging_connection,
-            prompt_provider=prompt_provider,
+            prompt_provider=BasicPromptProvider(system_prompt, task_prompt)
         )
 
     def transform_query(self, query: str, generation_config: GenerationConfig) -> list[str]:  # type: ignore
@@ -71,11 +65,11 @@ class SyntheticRAGPipeline(BasicRAGPipeline):
             "with information about X` or `readings that cover topic Y`.\n\n## Query:\n"
             "{query}\n\n## Response:".format(query=query)
         )
+        orig_stream = generation_config.stream
         generation_config.stream = False
         completion = self.generate_completion(prompt, generation_config)
-        transformed_queries = (
-            completion.choices[0].message.content.strip().split("\n")
-        )
+        transformed_queries = completion.choices[0].message.content.strip().split("\n")
+        generation_config.stream = orig_stream
         print(f"Transformed Queries: {transformed_queries}")
         return transformed_queries
 
@@ -107,8 +101,7 @@ class SyntheticRAGPipeline(BasicRAGPipeline):
         queries = [ele[0] for ele in results]
         search_results = [ele[1] for ele in results]
         reranked_results = [
-            self.rerank_results(search_result)
-            for search_result in search_results
+            self.rerank_results(search_result) for search_result in search_results
         ]
         context = ""
         offset = 1
@@ -125,6 +118,8 @@ class SyntheticRAGPipeline(BasicRAGPipeline):
         limit=5,
         search_only=False,
         generation_config: Optional[GenerationConfig] = None,
+        *args,
+        **kwargs,
     ):
         """
         Runs the completion pipeline.
@@ -132,6 +127,7 @@ class SyntheticRAGPipeline(BasicRAGPipeline):
         if not generation_config:
             generation_config = GenerationConfig(model="gpt-3.5-turbo")
 
+        print("generation_config = ", generation_config)
         self.initialize_pipeline(query, search_only)
         transformed_queries = self.transform_query(query, generation_config)
         search_results = [
@@ -139,13 +135,16 @@ class SyntheticRAGPipeline(BasicRAGPipeline):
             for transformed_query in transformed_queries
         ]
         if search_only:
+            print('returning here...')
             return RAGPipelineOutput(search_results, None, None)
 
         context = self.construct_context(search_results)
         prompt = self.construct_prompt({"query": query, "context": context})
 
+        print('generation_config.stream = ', generation_config.stream)
         if not generation_config.stream:
             completion = self.generate_completion(prompt, generation_config)
+            print('returning there...')
             return RAGPipelineOutput(search_results, context, completion)
 
         return self._stream_run(
@@ -162,7 +161,26 @@ class SyntheticRAGPipeline(BasicRAGPipeline):
         return context
 
 
+    def _stream_run(
+        self,
+        search_results: list,
+        context: str,
+        prompt: str,
+        generation_config: GenerationConfig,
+    ) -> Generator[str, None, None]:
+        yield f"<{RAGPipeline.SEARCH_STREAM_MARKER}>"
+        yield json.dumps([ele[1][0].to_dict() for ele in search_results])
+        yield f"</{RAGPipeline.SEARCH_STREAM_MARKER}>"
+
+        yield f"<{RAGPipeline.CONTEXT_STREAM_MARKER}>"
+        yield context
+        yield f"</{RAGPipeline.CONTEXT_STREAM_MARKER}>"
+        yield f"<{RAGPipeline.COMPLETION_STREAM_MARKER}>"
+        for chunk in self.generate_completion(prompt, generation_config):
+            yield chunk
+        yield f"</{RAGPipeline.COMPLETION_STREAM_MARKER}>"
+
+
+
 # Creates a pipeline using the `E2EPipelineFactory`
-app = E2EPipelineFactory.create_pipeline(
-    config=R2RConfig.load_config(), rag_pipeline_impl=SyntheticRAGPipeline
-)
+app = E2EPipelineFactory.create_pipeline(rag_pipeline_impl=SyntheticRAGPipeline, config=R2RConfig.load_config("config.json"))
