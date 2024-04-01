e63ac475e007b883e61e3cacb477d149aff4cfe8
emrgnt-cmplxty <68796651+emrgnt-cmplxty@users.noreply.github.com>
2024-03-31 22:03:42 -0700

    Update client.py

--- r2r/vecs/client.py
+++ r2r/vecs/client.py
@@ -69,8 +69,21 @@ class Client:
 
         with self.Session() as sess:
             with sess.begin():
-                sess.execute(text("create schema if not exists vecs;"))
-                sess.execute(text("create extension if not exists vector;"))
+                try:
+                    sess.execute(text("create schema if not exists vecs;"))
+                except Exception as e:
+                    if "already exists" not in str(e):
+                        raise ValueError(
+                            f"Error {e} occurred while creating the 'vecs' schema."
+                        )
+    
+                try:
+                    sess.execute(text("create extension if not exists vector;"))
+                except Exception as e:
+                    if "already exists" not in str(e):
+                        raise ValueError(
+                            f"Error {e} occurred while creating the 'vector' extension."
+                        )
                 self.vector_version: str = sess.execute(
                     text(
                         "select installed_version from pg_available_extensions where name = 'vector' limit 1;"
