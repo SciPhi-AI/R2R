1b81b61660abbf583110b769151e4ef43766edc0
unknown <mohamed.ali@redwoodcompliance.com>
2024-04-02 02:31:18 +0200

    adding llama cpp support

--- r2r/llms/__init__.py
+++ r2r/llms/__init__.py
@@ -1,4 +1,12 @@
 from .litellm.base import LiteLLM, LiteLLMConfig
+from .llamacpp.base import LlamaCPP, LlamaCppConfig
 from .openai.base import OpenAIConfig, OpenAILLM
 
-__all__ = ["LiteLLMConfig", "LiteLLM", "OpenAIConfig", "OpenAILLM"]
+__all__ = [
+    "LiteLLMConfig",
+    "LiteLLM",
+    "OpenAIConfig",
+    "OpenAILLM",
+    "LlamaCPP",
+    "LlamaCppConfig",
+]
