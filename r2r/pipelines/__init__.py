90a6db8591a1b7e5ae1aa059c8c767c7933ee5e7
emrgnt-cmplxty <owen@algofi.org>
2024-04-16 17:58:33 -0700

    remove erroneous export

--- r2r/pipelines/__init__.py
+++ r2r/pipelines/__init__.py
@@ -1,4 +1,4 @@
-from .basic.embedding import BasicEmbeddingPipeline, DocumentPage
+from .basic.embedding import BasicEmbeddingPipeline
 from .basic.eval import BasicEvalPipeline
 from .basic.ingestion import BasicIngestionPipeline, IngestionType
 from .basic.prompt_provider import BasicPromptProvider
@@ -6,7 +6,6 @@ from .basic.rag import BasicRAGPipeline
 from .web_search.rag import WebRAGPipeline
 
 __all__ = [
-    "DocumentPage",
     "BasicEmbeddingPipeline",
     "BasicEvalPipeline",
     "IngestionType",
