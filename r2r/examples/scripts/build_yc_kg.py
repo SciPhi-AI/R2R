b0d722ec8bc666e6e5618b02ec277e351f3f35ee
emrgnt-cmplxty <owen@algofi.org>
2024-06-11 21:28:51 -0700

    up

--- r2r/examples/scripts/build_yc_kg.py
+++ r2r/examples/scripts/build_yc_kg.py
@@ -1,12 +1,13 @@
 import os
 
+import fire
 import requests
 from bs4 import BeautifulSoup, Comment
 
 from r2r import Document, R2RAppBuilder, R2RConfig, generate_id_from_label
 
 yc_extraction_notes = """
-Notes:\n  - For this specific task, you are working with company data from the YC directory. You sould seek to extract as many details about the company as possible (name, founders, CEO/CTO/..., URLs, group partner, YC batch (S23, W24, ..), key tags (b2b, developer tools, artificial intelligence), emails, etc.\n\n### Response:\n
+Notes:\n  - For this specific task, you are working with company data from the YC directory. You should seek to extract as many details about the company as possible (name, founders, CEO/CTO/..., URLs, group partner, YC batch (S23, W24, ..), key tags (b2b, developer tools, artificial intelligence), emails, etc.\n\n### Response:\n
 """
 
 
@@ -72,6 +73,13 @@ def fetch_and_clean_yc_co_data(url):
         return "Main content not found"
 
 
+def execute_query(provider, query, params={}):
+    print(f"Executing query: {query}")
+    with provider.client.session(database=provider._database) as session:
+        result = session.run(query, params)
+        return [record.data() for record in result]
+
+
 def delete_all_entries(provider):
     delete_query = "MATCH (n) DETACH DELETE n;"
     with provider.client.session(database=provider._database) as session:
@@ -92,7 +100,7 @@ def print_all_relationships(provider):
             )
 
 
-if __name__ == "__main__":
+def main(max_entries=50, delete=False):
     # Load the R2R configuration and build the app
     config = R2RConfig.from_json()
     r2r = R2RAppBuilder(config).build()
@@ -107,23 +115,23 @@ if __name__ == "__main__":
         ner_kg_prompt.split("### Response:")[0] + yc_extraction_notes
     )
 
-
-    # Optional - clear the graph
-    # -- USE WITH CARE -- this will delete all entries in the graph
-    delete_all_entries(kg_provider)
+    # Optional - clear the graph if the delete flag is set
+    if delete:
+        delete_all_entries(kg_provider)
 
     r2r.providers.prompt.update_prompt("ner_kg_extraction", yc_ner_kg_prompt)
 
     url_map = get_all_yc_co_directory_urls()
 
-    # Ingest and clean the data for each company
     i = 0
+    # Ingest and clean the data for each company
     for company, url in url_map.items():
-        print(f"Company: {company}, URL: {url}")
-        print("text = ", fetch_and_clean_yc_co_data(url))
+        print(f"Ingesting company: {company}, from url: {url}")
 
         company_data = fetch_and_clean_yc_co_data(url)
-
+        i += 1
+        if i >= max_entries:
+            break
         # Ingest as a text document
         r2r.ingest_documents(
             [
@@ -135,26 +143,9 @@ if __name__ == "__main__":
                 )
             ]
         )
-        if i >= 5:
-            break
-        i+=1
 
     print_all_relationships(kg_provider)
 
-# Optional - clear the graph
-# -- USE WITH CARE -- this will delete all entries in the graph
-# delete_all_entries(kg_provider)
 
-# # Ingest the document
-# r2r.ingest_documents(
-#     [
-#         Document(
-#             id=generate_id_from_label('airbnb'),
-#             type="txt",
-#             data=input_text,
-#             metadata={},
-#         )
-#     ]
-# )
-
-# print_all_relationships(kg_provider)
+if __name__ == "__main__":
+    fire.Fire(main)
