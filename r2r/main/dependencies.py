8ae9e4a92f1490aaad899539fc78b08791b8b42f
emrgnt-cmplxty <68796651+emrgnt-cmplxty@users.noreply.github.com>
2024-06-23 14:30:38 -0700

    fix args and kwargs dev (#520)

--- r2r/main/dependencies.py
+++ r2r/main/dependencies.py
@@ -6,9 +6,9 @@ from .assembly.builder import R2RAppBuilder
 r2r_app_instance = None
 
 
-def get_r2r_app(app_builder: Optional[str] = None, *args, **kwargs):
+def get_r2r_app(app_builder: Optional[str] = None):
     global r2r_app_instance
     if r2r_app_instance is None:
         builder = app_builder or R2RAppBuilder()
-        r2r_app_instance = builder.build(*args, **kwargs)
+        r2r_app_instance = builder.build()
     return r2r_app_instance
