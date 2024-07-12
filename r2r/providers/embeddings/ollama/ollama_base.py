6286dfc0828a980da7590da6ee5cfcdab977c12e
emrgnt-cmplxty <68796651+emrgnt-cmplxty@users.noreply.github.com>
2024-07-12 07:05:23 -0700

    Revert "draft prefixes (#679)" (#681)
    This reverts commit 24ad8359209b7c40a833e1cc04c705f1aa1f760b.

--- r2r/providers/embeddings/ollama/ollama_base.py
+++ r2r/providers/embeddings/ollama/ollama_base.py
@@ -6,21 +6,9 @@ from typing import Any
 
 from ollama import AsyncClient, Client
 
-from r2r.base import EmbeddingConfig, EmbeddingProvider, VectorSearchResult, EmbeddingPurpose
+from r2r.base import EmbeddingConfig, EmbeddingProvider, VectorSearchResult
 
 logger = logging.getLogger(__name__)
-
-
-default_embedding_prefixes = {
-    'nomic-embed-text-v1.5': {
-        EmbeddingPurpose.INDEX: 'search_query: ',
-        EmbeddingPurpose.DOCUMENT: 'search_document: ',
-    },
-    'nomic-embed-text': {
-        EmbeddingPurpose.INDEX: 'search_query: ',
-        EmbeddingPurpose.DOCUMENT: 'search_document: ',
-    },
-}
 
 
 class OllamaEmbeddingProvider(EmbeddingProvider):
@@ -55,13 +43,6 @@ class OllamaEmbeddingProvider(EmbeddingProvider):
         self.max_backoff = 60
         self.concurrency_limit = 10
         self.semaphore = asyncio.Semaphore(self.concurrency_limit)
-        set_prefixes(config.prefixes or {}, self.base_model)
-
-    def set_prefixes(self, prefixes: dict[EmbeddingPurpose, str], base_model: str):
-        self.prefixes = {}
-        if base_model and (base_model in default_embedding_prefixes):
-            for t, p in default_embedding_prefixes[base_model].items():
-                self.prefixes[t] = p
 
     async def process_queue(self):
         while True:
@@ -103,13 +84,11 @@ class OllamaEmbeddingProvider(EmbeddingProvider):
         self,
         text: str,
         stage: EmbeddingProvider.PipeStage = EmbeddingProvider.PipeStage.BASE,
-        purpose: EmbeddingPurpose = EmbeddingPurpose.INDEX,
     ) -> list[float]:
         if stage != EmbeddingProvider.PipeStage.BASE:
             raise ValueError(
                 "OllamaEmbeddingProvider only supports search stage."
             )
-        text = self.prefixes.get(purpose, '') + text
 
         try:
             response = self.client.embeddings(
@@ -131,7 +110,6 @@ class OllamaEmbeddingProvider(EmbeddingProvider):
         self,
         texts: list[str],
         stage: EmbeddingProvider.PipeStage = EmbeddingProvider.PipeStage.BASE,
-        purpose: EmbeddingPurpose = EmbeddingPurpose.INDEX,
     ) -> list[list[float]]:
         if stage != EmbeddingProvider.PipeStage.BASE:
             raise ValueError(
@@ -141,7 +119,6 @@ class OllamaEmbeddingProvider(EmbeddingProvider):
         queue_processor = asyncio.create_task(self.process_queue())
         futures = []
         for text in texts:
-            text = self.prefixes.get(purpose, '') + text
             future = asyncio.Future()
             await self.request_queue.put({"text": text, "future": future})
             futures.append(future)
