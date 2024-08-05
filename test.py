ef80dbf68e409e03b9be374d4cb19741ee7880d9
NolanTrem <34580718+NolanTrem@users.noreply.github.com>
2024-08-05 15:33:06 -0700

    Add user id for search and rag

--- test.py
+++
@@ -1,9 +0,0 @@
-from r2r import R2RClient
-
-client = R2RClient("http://localhost:8000")
-
-login_response = client.login("admin@example.com", "change_me_immediately")
-print(login_response)
-
-search_response = client.search("What was Uber's profit in 2020?")
-print(search_response)
