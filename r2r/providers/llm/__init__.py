e87e4b9dd6673043fd61e89374ffdbd374d4f28d
emrgnt-cmplxty <68796651+emrgnt-cmplxty@users.noreply.github.com>
2024-08-06 12:42:21 -0700

    Revert "Feature/cloud fork p1 (#851)"
    This reverts commit 8ee0c6abfe934cac531e91e873bfc81654a3f82d.

--- r2r/providers/llm/__init__.py
+++ r2r/providers/llm/__init__.py
@@ -1,9 +1,7 @@
 from .litellm import LiteCompletionProvider
 from .openai import OpenAICompletionProvider
-from .sciphi import SciPhiCompletionProvider
 
 __all__ = [
     "LiteCompletionProvider",
     "OpenAICompletionProvider",
-    "SciPhiCompletionProvider"
 ]
