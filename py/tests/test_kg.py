f84da73ce8d376b9857886ce26d8a6047b7b3c8e
NolanTrem <34580718+NolanTrem@users.noreply.github.com>
2024-08-21 10:22:56 -0700

    Fix CLI tests

--- py/tests/test_kg.py
+++ py/tests/test_kg.py
@@ -1,10 +1,11 @@
-import pytest
 import asyncio
 from unittest.mock import AsyncMock, MagicMock
-from core.pipes.kg.extraction import KGTriplesExtractionPipe
-from core.pipes.kg.clustering import KGClusteringPipe
-from core.base import DocumentFragment, KGExtraction, Entity, Triple, Community
 from uuid import uuid4
+
+import pytest
+from core.base import Community, DocumentFragment, Entity, KGExtraction, Triple
+from core.pipes.kg.clustering import KGClusteringPipe
+from core.pipes.kg.extraction import KGTriplesExtractionPipe
 
 
 @pytest.fixture
