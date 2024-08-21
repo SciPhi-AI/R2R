2906dcd0d00fdc6da3eec2ac125746436612f7ab
Nolan Tremelling <34580718+NolanTrem@users.noreply.github.com>
2024-08-21 16:05:31 -0700

    Fix bug in update files (#923)

--- py/setup.py
+++ py/setup.py
@@ -1,17 +1,19 @@
 import nltk
 from setuptools import setup
 
+
 def download_nltk_data():
-    nltk.download('wordnet', quiet=True)
+    nltk.download("wordnet", quiet=True)
+
 
 setup(
-    name='r2r',
-    version='0.3.0',
-    setup_requires=['nltk'],
+    name="r2r",
+    version="0.3.0",
+    setup_requires=["nltk"],
     cmdclass={
-        'install': lambda _: download_nltk_data(),
+        "install": lambda _: download_nltk_data(),
     },
 )
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     download_nltk_data()
