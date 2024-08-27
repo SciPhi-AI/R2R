8c85edd7b19d8e1be7cae1e40198ab854d544ed9
Shreyas Pimpalgaonkar <shreyas.gp.7@gmail.com>
2024-08-27 09:14:40 -0700

    up

--- py/core/base/abstractions/llm.py
+++ py/core/base/abstractions/llm.py
@@ -29,7 +29,7 @@ class RAGCompletion:
 
 class GenerationConfig(BaseModel):
     _defaults: ClassVar[dict] = {
-        "model": "openai/gpt-4o-mini",
+        "model": "openai/gpt-4o",
         "temperature": 0.1,
         "top_p": 1.0,
         "max_tokens_to_sample": 1024,
