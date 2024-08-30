ab25c0de0293cd71cd83addc884eb5ee3cdabcfe
emrgnt-cmplxty <owen@algofi.org>
2024-08-30 16:51:08 -0700

    cleanups

--- py/core/main/hatchet/restructure_workflow.py
+++ py/core/main/hatchet/restructure_workflow.py
@@ -1,11 +1,13 @@
 from hatchet_sdk import Context
 
-from core.base import Entity, KGExtraction
-
 from ..services import RestructureService, RestructureServiceAdapter
 from .base import r2r_hatchet
 
-
+# TODO - 
+# 1. Add status tracking for each step
+# 2. Add error handling for each step
+# 3. Add intelligent fan-out across the workflow
+# e.g. per document extractions -> chunked node creation -> clustering
 @r2r_hatchet.workflow(
     name="enrich-graph", on_events=["graph:enrich"], timeout=3600
 )
@@ -38,42 +40,18 @@ class EnrichGraphWorkflow:
     async def kg_node_creation(self, context: Context) -> None:
         print("kg node extraction ingress...")
         prev_step = context.step_output("kg_extract_and_store")
-        # Nodes are not explicitly needed
         await self.restructure_service.kg_node_creation(prev_step["result"])
-        # print('node extraction nodes = ', nodes)
         return {"result": None}
-
-    # @r2r_hatchet.step(retries=3, parents=["kg_node_extraction"])
-    # async def kg_node_description(self, context: Context) -> None:
-    #     prev_step = context.step_output("kg_node_extraction")
-    #     descriptions = await self.restructure_service.kg_node_description(prev_step["result"])
-    #     return {"result": descriptions}
 
     @r2r_hatchet.step(retries=3, parents=["kg_node_creation"])
     async def kg_clustering(self, context: Context) -> None:
-        # prev_step = context.step_output("kg_node_creation")
         input_data = context.workflow_input()["request"]
-        # print('entity = ', prev_step["result"][0])
-        # entities = [Entity.from_json(entity) for entity in prev_step["result"]]
         parsed_data = RestructureServiceAdapter.parse_enrich_graph_input(
             input_data
         )
         kg_enrichment_settings = parsed_data["kg_enrichment_settings"]
 
-        clusters = await self.restructure_service.kg_clustering(
+        await self.restructure_service.kg_clustering(
             kg_enrichment_settings
         )
-        print("clusters = ", clusters)
         return {"result": None}
-
-    # @r2r_hatchet.step(retries=3, parents=["kg_clustering"])
-    # async def finalize_enrichment(self, context: Context) -> None:
-    #     prev_step = context.step_output("kg_clustering")
-    #     clusters = prev_step["result"]
-
-    #     # Process the clusters as needed
-    #     result = []
-    #     async for cluster in clusters:
-    #         result.append(cluster)
-
-    #     return {"result": result}        # storage = await self.restructure_service.kg_s/torage(triples)
