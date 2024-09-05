682c73ee007068c119cb2fe2376d0909968e83a0
Shreyas Pimpalgaonkar <shreyas.gp.7@gmail.com>
2024-09-05 14:23:20 -0700

    up

--- py/core/base/api/models/restructure/responses.py
+++ py/core/base/api/models/restructure/responses.py
@@ -9,9 +9,26 @@ class KGCreationResponse(BaseModel):
     message: str
     task_id: UUID
 
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "message": "Graph creation queued successfully.",
+                "task_id": "c68dc72e-fc23-5452-8f49-d7bd46088a96",
+            }
+        }
+
 class KGEnrichmentResponse(BaseModel):
     message: str
     task_id: UUID
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "message": "Graph enrichment queued successfuly.",
+                "task_id": "c68dc72e-fc23-5452-8f49-d7bd46088a96",
+            }
+        }
+
 
 WrappedKGCreationResponse = ResultsWrapper[KGCreationResponse]
 WrappedKGEnrichmentResponse = ResultsWrapper[KGEnrichmentResponse]
