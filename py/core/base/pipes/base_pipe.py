0d8be5e8179c799539d8dae097a5b246358944e7
emrgnt-cmplxty <owen@algofi.org>
2024-09-10 18:14:28 -0700

    bump the project

--- py/core/base/pipes/base_pipe.py
+++ py/core/base/pipes/base_pipe.py
@@ -68,7 +68,7 @@ class AsyncPipe:
     class PipeConfig(BaseModel):
         """Configuration for a pipe."""
 
-        name: str = "default_strategy"
+        name: str = "default_pipe"
         max_log_queue_size: int = 100
 
         class Config:
