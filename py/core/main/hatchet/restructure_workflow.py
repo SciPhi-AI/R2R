816da96cea25ba2fa99df459fc77d0a71003b7ec
Shreyas Pimpalgaonkar <shreyas.gp.7@gmail.com>
2024-09-11 07:29:14 -0700

    modify document_ids

--- py/core/main/hatchet/restructure_workflow.py
+++ py/core/main/hatchet/restructure_workflow.py
@@ -84,6 +84,7 @@ class CreateGraphWorkflow:
         document_ids = input_data.get("document_ids", [])
 
         # check if graph was created for each document id
+        document_ids = [uuid.UUID(doc_id) for doc_id in document_ids]
         documents_overviews = self.restructure_service.providers.database.relational.get_documents_overview(
             filter_document_ids=document_ids
         )
