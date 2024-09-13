355138f68ec2e5929d17611c0bb339a151a792e4
Shreyas Pimpalgaonkar <shreyas.gp.7@gmail.com>
2024-09-13 16:11:05 -0700

    up

--- py/core/integrations/unstructured/main.py
+++ py/core/integrations/unstructured/main.py
@@ -5,7 +5,7 @@ from io import BytesIO
 import asyncio
 import concurrent.futures
 import os
-
+import base64
 from unstructured.partition.auto import partition
 
 app = FastAPI()
@@ -19,8 +19,9 @@ class PartitionResponseModel(BaseModel):
 
 executor = concurrent.futures.ThreadPoolExecutor(max_workers=os.environ.get("MAX_INGESTION_WORKERS", 10))
 
-def run_partition(file_content: bytes, chunking_config: Dict) -> List[Dict]:
-    file_io = BytesIO(file_content)
+def run_partition(file_content: str, chunking_config: Dict) -> List[Dict]:
+    file_content_bytes = base64.b64decode(file_content)
+    file_io = BytesIO(file_content_bytes)
     elements = partition(file=file_io, **chunking_config)
     return [element.to_dict() for element in elements]
 
@@ -38,6 +39,7 @@ async def partition_endpoint(request: PartitionRequestModel):
             request.file_content,
             request.chunking_config,
         )
+
         return PartitionResponseModel(elements=elements)
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
