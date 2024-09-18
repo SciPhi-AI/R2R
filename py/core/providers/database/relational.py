d1efb499a16cad870d7fd31454d9a507ef3eb397
emrgnt-cmplxty <68796651+emrgnt-cmplxty@users.noreply.github.com>
2024-09-17 17:16:56 -0700

    fix pr (#1192)

--- py/core/providers/database/relational.py
+++ py/core/providers/database/relational.py
@@ -7,7 +7,7 @@ from core.providers.database.base import DatabaseMixin
 from core.providers.database.document import DocumentMixin
 from core.providers.database.tokens import BlacklistedTokensMixin
 from core.providers.database.user import UserMixin
-from py.core.providers.database.collection import CollectionMixin
+from core.providers.database.collection import CollectionMixin
 
 logger = logging.getLogger(__name__)
 
