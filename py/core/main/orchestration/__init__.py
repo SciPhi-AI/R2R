e7a12b47e9653664ddc5da5339bdde4bcf28af57
emrgnt-cmplxty <owen@algofi.org>
2024-09-25 15:27:10 -0700

    fully encapsulate orchestration

---
+++ py/core/main/orchestration/__init__.py
@@ -0,0 +1,7 @@
+from .hatchet.ingestion_workflow import hatchet_ingestion_workflow
+from .hatchet.restructure_workflow import hatchet_restructure_workflow
+
+__all__ = [
+    "hatchet_ingestion_workflow",
+    "hatchet_restructure_workflow",
+]
