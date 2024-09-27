b8e17a396e2b76653781c804364282036e5809bc
Shreyas Pimpalgaonkar <shreyas.gp.7@gmail.com>
2024-09-27 12:21:58 -0700

    change api

--- py/cli/commands/kg.py
+++ py/cli/commands/kg.py
@@ -9,21 +9,14 @@ from cli.utils.timer import timer
 
 @cli.command()
 @click.option(
+    "--project-name",
+    required=True,
+    help="Project name to create graph for.",
+)
+@click.option(
     "--collection-id",
     required=True,
     help="Collection ID to create graph for.",
-)
-@click.option(
-    "--document-ids",
-    required=False,
-    default=None,
-    help="Document IDs to create graph for (comma-separated)",
-)
-@click.option(
-    "--force-enrichment",
-    required=False,
-    default=False,
-    help="Force enrichment of the graph even if graph creation is still in progress for some documents.",
 )
 @click.option(
     "--skip-clustering",
@@ -33,19 +26,38 @@ from cli.utils.timer import timer
 )
 @pass_context
 def create_graph(
-    ctx, collection_id, document_ids, force_enrichment, skip_clustering
+    ctx, project_name, collection_id, force_enrichment, skip_clustering
 ):
     """
     Create a new graph.
     """
     client = ctx.obj
     with timer():
-        if document_ids is None:
-            document_ids = []
-        else:
-            document_ids = document_ids.split(",")
         response = client.create_graph(
-            collection_id, document_ids, force_enrichment, skip_clustering
+            project_name, collection_id, force_enrichment, skip_clustering
         )
+
+    click.echo(json.dumps(response, indent=2))
+
+@cli.command()
+@click.option(
+    "--collection-id",
+    required=True,
+    help="Collection ID to enrich graph for.",
+)
+@click.option(
+    "--force-enrichment",
+    required=False,
+    default=False,
+    help="Force enrichment of the graph even if graph creation is still in progress for some documents.",
+)
+@pass_context
+def enrich_graph(ctx, collection_id):
+    """
+    Enrich an existing graph.
+    """
+    client = ctx.obj
+    with timer():
+        response = client.enrich_graph(collection_id)
 
     click.echo(json.dumps(response, indent=2))
