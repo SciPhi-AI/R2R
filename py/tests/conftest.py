1961b2cfea284252fd71630edc285edd42c9556b
emrgnt-cmplxty <owen@algofi.org>
2024-10-04 14:37:31 -0700

    add pgvector

--- py/tests/conftest.py
+++ py/tests/conftest.py
@@ -64,7 +64,7 @@ def sample_entries(dimension, num_entries):
 def app_config():
     collection_id = uuid.uuid4()
 
-    random_project_name = f"_{collection_id.hex}_test_project"
+    random_project_name = f"a_{collection_id.hex}_test_project"
     print("random_project_name", random_project_name)
 
     return AppConfig(project_name=random_project_name)
