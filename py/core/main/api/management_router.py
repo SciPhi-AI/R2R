f9f6eadd7307a76b4826fcc7fed9288d412294db
NolanTrem <34580718+NolanTrem@users.noreply.github.com>
2024-10-04 17:59:27 -0700

    Don't throw an error when deleting a collection with no documents, fix return on create collection, more js tests

--- py/core/main/api/management_router.py
+++ py/core/main/api/management_router.py
@@ -436,9 +436,10 @@ class ManagementRouter(BaseRouter):
             collection_id = await self.service.create_collection(
                 name, description
             )
-            return await self.service.add_user_to_collection(  # type: ignore
+            await self.service.add_user_to_collection(  # type: ignore
                 auth_user.id, collection_id.collection_id
             )
+            return collection_id
 
         @self.router.get("/get_collection/{collection_id}")
         @self.base_endpoint
