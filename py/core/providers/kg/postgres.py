acf09eeb6e5762e862ddc5f567ed9db581fe4172
Shreyas Pimpalgaonkar <shreyas.gp.7@gmail.com>
2024-10-24 10:34:09 -0700

    up

--- py/core/providers/kg/postgres.py
+++ py/core/providers/kg/postgres.py
@@ -490,10 +490,15 @@ class PostgresKGProvider(KGProvider):
             filter_query = "WHERE collection_id = ANY($3)"
             filter_ids = collection_ids_dict["$overlap"]
 
-            if search_type == "__Community__":
+            if (
+                search_type == "__Community__"
+                or table_name == "collection_entity"
+            ):
                 logger.info(f"Searching in collection ids: {filter_ids}")
-
-            if search_type in ["__Entity__", "__Relationship__"]:
+            elif (
+                search_type == "__Entity__"
+                or search_type == "__Relationship__"
+            ):
                 filter_query = "WHERE document_id = ANY($3)"
                 # TODO - This seems like a hack, we will need a better way to filter by collection ids for entities and relationships
                 query = f"""
@@ -509,7 +514,7 @@ class PostgresKGProvider(KGProvider):
             SELECT {property_names_str} FROM {self._get_table_name(table_name)} {filter_query} ORDER BY {embedding_type} <=> $1 LIMIT $2;
         """
 
-        if not filter_query:
+        if filter_query != "" and filter_ids:
             results = await self.fetch_query(
                 QUERY, (str(query_embedding), limit, filter_ids)
             )
