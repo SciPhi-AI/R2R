c58257cb80d6dcbe7f6b998d62e539d13d69e4ed
emrgnt-cmplxty <68796651+emrgnt-cmplxty@users.noreply.github.com>
2024-11-06 17:54:09 -0800

    Feature/add graph to v3 (#1565)
    * complete simple tests, cleanup routers
    
    * up

--- py/core/main/services/kg_service.py
+++ py/core/main/services/kg_service.py
@@ -316,6 +316,24 @@ class KgService(Service):
             collection_id, kg_enrichment_settings
         )
 
+    @telemetry_event("list_entities")
+    async def list_entities(
+        self,
+        collection_id: Optional[UUID] = None,
+        entity_ids: Optional[list[str]] = None,
+        entity_table_name: str = "document_entity",
+        offset: Optional[int] = None,
+        limit: Optional[int] = None,
+        **kwargs,
+    ):
+        return await self.providers.database.get_entities(
+            collection_id=collection_id,
+            entity_ids=entity_ids,
+            entity_table_name=entity_table_name,
+            offset=offset or 0,
+            limit=limit or -1,
+        )
+
     @telemetry_event("get_entities")
     async def get_entities(
         self,
@@ -352,6 +370,23 @@ class KgService(Service):
             limit=limit or -1,
         )
 
+    @telemetry_event("list_triples")
+    async def list_triples(
+        self,
+        collection_id: Optional[UUID] = None,
+        entity_names: Optional[list[str]] = None,
+        triple_ids: Optional[list[str]] = None,
+        offset: Optional[int] = None,
+        limit: Optional[int] = None,
+        **kwargs,
+    ):
+        return await self.providers.database.get_triples(
+            collection_id=collection_id,
+            entity_names=entity_names,
+            triple_ids=triple_ids,
+            offset=offset or 0,
+            limit=limit or -1,
+        )
     @telemetry_event("get_communities")
     async def get_communities(
         self,
@@ -369,6 +404,25 @@ class KgService(Service):
             offset=offset or 0,
             limit=limit or -1,
         )
+
+    @telemetry_event("list_communities")
+    async def list_communities(
+        self,
+        collection_id: Optional[UUID] = None,
+        levels: Optional[list[int]] = None,
+        community_numbers: Optional[list[int]] = None,
+        offset: Optional[int] = None,
+        limit: Optional[int] = None,
+        **kwargs,
+    ):
+        return await self.providers.database.get_communities(
+            collection_id=collection_id,
+            levels=levels,
+            community_numbers=community_numbers,
+            offset=offset or 0,
+            limit=limit or -1,
+        )
+
 
     @telemetry_event("get_deduplication_estimate")
     async def get_deduplication_estimate(
