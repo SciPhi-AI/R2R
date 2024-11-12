b8f9ce5996556cc9e9bcc27c4ddc091d8511bea0
NolanTrem <34580718+NolanTrem@users.noreply.github.com>
2024-11-12 11:55:15 -0800

    Jest config

--- js/sdk/jest.config.js
+++ js/sdk/jest.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
+  testRunner: 'jest-circus/runner',
+  maxWorkers: 1,
   testMatch: [
     "**/__tests__/**/*.ts?(x)",
     "**/__tests__/**/?(*.)+(spec|test).ts?(x)",
