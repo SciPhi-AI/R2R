cd284371a8efb39e27a08d52f47037b2edd006c4
NolanTrem <34580718+NolanTrem@users.noreply.github.com>
2024-11-25 12:38:43 -0600

    Increase test timeout

--- js/sdk/__tests__/r2rClientIntegrationUser.test.ts
+++ js/sdk/__tests__/r2rClientIntegrationUser.test.ts
@@ -148,13 +148,13 @@ describe("r2rClient Integration Tests", () => {
         metadatas: [{ title: "raskolnikov.txt" }],
       }),
     ).resolves.not.toThrow();
-  });
+  }, 30000);
 
   test("Ingest files in folder", async () => {
     const files = ["examples/data/folder"];
 
     await expect(client.ingestFiles(files)).resolves.not.toThrow();
-  });
+  }, 30000);
 
   test("Update files", async () => {
     const updated_file = [
@@ -165,7 +165,7 @@ describe("r2rClient Integration Tests", () => {
         document_ids: ["0b80081e-a37a-579f-a06d-7d2032435d65"],
       }),
     ).resolves.not.toThrow();
-  });
+  }, 30000);
 
   test("Search documents", async () => {
     await expect(client.search("test")).resolves.not.toThrow();
