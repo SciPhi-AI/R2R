8e47daaf46cabf69a33948e2ac93c5163ee51058
emrgnt-cmplxty <owen@algofi.org>
2025-01-15 10:58:11 -0800

    fix client

--- py/sdk/async_client.py
+++ py/sdk/async_client.py
@@ -50,6 +50,12 @@ class R2RAsyncClient(BaseClient):
         self, method: str, endpoint: str, version: str = "v3", **kwargs
     ):
         url = self._get_full_url(endpoint, version)
+        if "https://api.cloud.sciphi.ai" in url:
+            if not self.access_token and not self.api_key:
+                raise R2RException(
+                    status_code=401,
+                    message="Access token or api key is required to access `https://api.cloud.sciphi.ai`. To change the base url, use `set_base_url` method. For instance, if using the CLI then execute `r2r set-api-base http://localhost:7272`, or set the local environment variable `R2R_API_BASE` to `http://localhost:7272`.",
+                )
         request_args = self._prepare_request_args(endpoint, **kwargs)
 
         try:
@@ -64,6 +70,7 @@ class R2RAsyncClient(BaseClient):
                 return BytesIO(response.content)
 
         except httpx.RequestError as e:
+            print('e = ', e)
             raise R2RException(
                 status_code=500,
                 message=f"Request failed: {str(e)}",
