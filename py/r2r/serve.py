afa9da91b4314fbd244a6a36472648f2516c818d
NolanTrem <34580718+NolanTrem@users.noreply.github.com>
2025-02-06 21:39:25 -0800

    Add support for directly serving

---
+++ py/r2r/serve.py
@@ -0,0 +1,50 @@
+import asyncio
+import os
+from typing import Optional
+
+from core import R2RApp, R2RBuilder, R2RConfig
+from core.utils.logging_config import configure_logging
+
+
+async def create_app(
+    config_name: Optional[str] = None,
+    config_path: Optional[str] = None,
+    full: bool = False,
+) -> "R2RApp":
+    config_name = config_name or os.getenv("R2R_CONFIG_NAME")
+    config_path = config_path or os.getenv("R2R_CONFIG_PATH")
+
+    if config_path and config_name:
+        raise ValueError("Cannot specify both config_path and config_name")
+    if not config_path and not config_name:
+        config_name = "full" if full else "default"
+
+    r2r_instance = await R2RBuilder(
+        config=R2RConfig.load(config_name, config_path)
+    ).build()
+
+    await r2r_instance.orchestration_provider.start_worker()
+    return r2r_instance
+
+
+def run_server(
+    host: str = "0.0.0.0",
+    port: int = 7272,
+    config_name: Optional[str] = None,
+    config_path: Optional[str] = None,
+    full: bool = False,
+):
+    configure_logging()
+
+    port = int(os.getenv("R2R_PORT", port))
+    host = os.getenv("R2R_HOST", host)
+
+    async def start():
+        app = await create_app(config_name, config_path, full)
+        await app.serve(host, port)
+
+    asyncio.run(start())
+
+
+if __name__ == "__main__":
+    run_server()
