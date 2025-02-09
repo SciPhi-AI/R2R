e6bd7e1c535a031a3472457e509baf07cf1982a3
NolanTrem <34580718+NolanTrem@users.noreply.github.com>
2025-02-09 14:12:19 -0800

    Fix compose comman ds

--- py/sdk/async_client.py
+++ py/sdk/async_client.py
@@ -112,7 +112,6 @@ class R2RAsyncClient(BaseClient):
                 message = response.text
             except Exception as e:
                 message = str(e)
-                print(e)
 
             raise R2RException(
                 status_code=response.status_code, message=message
