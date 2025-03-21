01d1442d97de94d9e7a954c20594d44a2156108d
emrgnt-cmplxty <owen@algofi.org>
2025-03-21 05:21:22 -0700

    fix sdk imports and citations error

--- py/r2r/__init__.py
+++ py/r2r/__init__.py
@@ -3,6 +3,7 @@ from importlib import metadata
 from sdk.async_client import R2RAsyncClient
 from sdk.sync_client import R2RClient
 from shared import *
+from shared import __all__ as shared_all
 
 __version__ = metadata.version("r2r")
 
@@ -11,7 +12,7 @@ __all__ = [
     "R2RClient",
     "__version__",
     "R2RException",
-]
+] + shared_all
 
 
 def get_version():
--- py/shared/__init__.py
+++ py/shared/__init__.py
@@ -1,6 +1,7 @@
 from .abstractions import *
 from .abstractions import __all__ as abstractions_all
 from .api.models import *
+from .api.models import __all__ as api_models_all
 from .utils import *
 
-__all__ = abstractions_all
+__all__ = abstractions_all + api_models_all
