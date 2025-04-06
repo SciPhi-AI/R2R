2a0686b52023110a246cc2dbea57f3d48131e57e
Ufuk Yavuz <78134326+suysoftware@users.noreply.github.com>
2025-04-05 21:53:37 -0500

    Update ConversationsIntegrationUser.test.ts

--- js/sdk/__tests__/ConversationsIntegrationUser.test.ts
+++ js/sdk/__tests__/ConversationsIntegrationUser.test.ts
@@ -20,7 +20,7 @@ describe("r2rClient V3 Collections Integration Tests", () => {
 
     await client.users.login({
       email: "admin@example.com",
-      password: "change_me_immediately",
+      password: "123Neo456*",
     });
   });
 
