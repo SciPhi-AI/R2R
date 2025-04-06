2ce01d2cc249aae61013c59a16b1e41db6f99470
Ufuk Yavuz <78134326+suysoftware@users.noreply.github.com>
2025-04-05 22:06:33 -0500

    Update DocumentsAndCollectionsIntegrationUser.test.ts

--- js/sdk/__tests__/DocumentsAndCollectionsIntegrationUser.test.ts
+++ js/sdk/__tests__/DocumentsAndCollectionsIntegrationUser.test.ts
@@ -48,7 +48,7 @@ describe("r2rClient V3 System Integration Tests User", () => {
   test("Login as a user 1", async () => {
     const response = await user1Client.users.login({
       email: "user_1@example.com",
-      password: "change_me_immediately",
+      password: "123Neo456*",
     });
     expect(response.results).toBeDefined();
   });
@@ -56,7 +56,7 @@ describe("r2rClient V3 System Integration Tests User", () => {
   test("Register user 2", async () => {
     const response = await client.users.create({
       email: "user_2@example.com",
-      password: "change_me_immediately",
+      password: "123Neo456*",
     });
 
     user2Id = response.results.id;
@@ -68,7 +68,7 @@ describe("r2rClient V3 System Integration Tests User", () => {
   test("Login as a user 2", async () => {
     const response = await user2Client.users.login({
       email: "user_2@example.com",
-      password: "change_me_immediately",
+      password: "123Neo456*",
     });
     expect(response.results).toBeDefined();
   });
@@ -297,7 +297,7 @@ describe("r2rClient V3 System Integration Tests User", () => {
   test("Delete user 1", async () => {
     const response = await client.users.delete({
       id: user1Id,
-      password: "change_me_immediately",
+      password: "123Neo456*",
     });
     expect(response.results).toBeDefined();
   });
@@ -305,7 +305,7 @@ describe("r2rClient V3 System Integration Tests User", () => {
   test("Delete user 2", async () => {
     const response = await client.users.delete({
       id: user2Id,
-      password: "change_me_immediately",
+      password: "123Neo456*",
     });
     expect(response.results).toBeDefined();
   });
