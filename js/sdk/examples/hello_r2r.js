0bc96cb3eebd394ec2b5107df185604db59acde4
SUY <suy.register@gmail.com>
2025-04-08 23:56:13 -0500

    Update R2RClient initialization in various examples to use localhost for local development and adjust rate limits in configuration files for testing purposes.

--- js/sdk/examples/hello_r2r.js
+++ js/sdk/examples/hello_r2r.js
@@ -3,7 +3,7 @@ const { r2rClient } = require("r2r-js");
 
 // Create an account at SciPhi Cloud https://app.sciphi.ai and set an R2R_API_KEY environment variable
 // or set the base URL to your instance. E.g. r2rClient("http://localhost:7272")
-const client = new r2rClient("http://20.55.204.62:7272");
+const client = new r2rClient("http://localhost:7272");
 
 async function main() {
   const filePath = path.resolve(__dirname, "data/raskolnikov.txt");
