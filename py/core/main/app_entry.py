5f043c2c6dcbace5dd3b6294625e2aef0995ae96
Nolan Tremelling <34580718+NolanTrem@users.noreply.github.com>
2025-05-27 15:19:03 -0700

    Fix setting non-default project name (#2201)

--- py/core/main/app_entry.py
+++ py/core/main/app_entry.py
@@ -11,6 +11,7 @@ from fastapi.responses import JSONResponse
 from core.base import R2RException
 from core.utils.logging_config import configure_logging
 
+from .app import R2RApp
 from .assembly import R2RBuilder, R2RConfig
 from .middleware.project_schema import ProjectSchemaMiddleware
 
@@ -50,7 +51,7 @@ async def lifespan(app: FastAPI):
 async def create_r2r_app(
     config_name: Optional[str] = "default",
     config_path: Optional[str] = None,
-):
+) -> R2RApp:
     config = R2RConfig.load(config_name=config_name, config_path=config_path)
 
     if (
@@ -74,6 +75,12 @@ if not config_path and not config_name:
 host = os.getenv("R2R_HOST", os.getenv("HOST", "0.0.0.0"))
 port = int(os.getenv("R2R_PORT", "7272"))
 
+config = R2RConfig.load(config_name=config_name, config_path=config_path)
+
+project_name = (
+    os.getenv("R2R_PROJECT_NAME") or config.app.project_name or "default"
+)
+
 logging.info(
     f"Environment R2R_IMAGE: {os.getenv('R2R_IMAGE')}",
 )
@@ -84,7 +91,7 @@ logging.info(
     f"Environment R2R_CONFIG_PATH: {'None' if config_path is None else config_path}"
 )
 logging.info(f"Environment R2R_PROJECT_NAME: {os.getenv('R2R_PROJECT_NAME')}")
-
+logging.info(f"Using project name: {project_name}")
 logging.info(
     f"Environment R2R_POSTGRES_HOST: {os.getenv('R2R_POSTGRES_HOST')}"
 )
@@ -125,7 +132,8 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 app.add_middleware(
     ProjectSchemaMiddleware,
-    default_schema="r2r_default",
+    default_schema=project_name,
 )
